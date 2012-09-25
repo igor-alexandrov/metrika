66da4c28cac99668fc114e4ec98e817c45f9792b
Igor Alexandrov <igor.alexandrov@gmail.com>
2012-09-25 17:45:50 +0400

    Added ability to manage counter goals

--- lib/metrika/api/methods.rb
+++ lib/metrika/api/methods.rb
@@ -1,9 +1,15 @@
 module Metrika
   module Api
     module Methods  
+
+      # Counters
       def get_counters
-        self.get(self.counters_path)['counters']        
+        self.get(self.counters_path)['counters']
       end      
+
+      def create_counter(params)
+        self.post(self.counters_path, params)['counter']        
+      end
 
       def counters_path
         "/counters"
@@ -11,11 +17,7 @@ module Metrika
 
       def get_counter(id)
         self.get(self.counter_path(id))['counter']        
-      end
-
-      def create_counter(params)
-        self.post(self.counters_path, params)['counter']        
-      end
+      end      
 
       def update_counter(id, params)
         self.put(self.counter_path(id), params)['counter']
@@ -35,7 +37,36 @@ module Metrika
 
       def counter_check_path(id)
         "/counter/#{id}/check"
+      end  
+
+      # Counter goals
+      def get_counter_goals(counter_id)
+        self.get(self.counter_goals_path(counter_id))['goals']
+      end
+
+      def create_counter_goal(counter_id, params)
+        self.post(self.counter_goals_path(counter_id), params)['goals']
+      end
+
+      def counter_goals_path(counter_id)
+        "/counter/#{counter_id}/goals"
+      end
+
+      def get_counter_goal(counter_id, id)
+        self.get(self.counter_goal_path(counter_id, id))['goal']        
       end      
+
+      def update_counter_goal(counter_id, id, params)
+        self.put(self.counter_goal_path(counter_id, id), params)['goal']
+      end
+
+      def delete_counter_goal(counter_id, id)
+        self.delete(self.counter_goal_path(counter_id, id))['goal']
+      end
+
+      def counter_goal_path(counter_id, id)
+        "/counter/#{counter_id}/goal/#{id}"
+      end
     end
   end
 end

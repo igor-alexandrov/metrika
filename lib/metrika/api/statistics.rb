f91006a95efb84d55e453012867bd2215c82cb36
Igor Alexandrov <igor.alexandrov@gmail.com>
2012-09-26 11:23:56 +0400

    All statistics reports implemented

--- lib/metrika/api/statistics.rb
+++ lib/metrika/api/statistics.rb
@@ -133,6 +133,210 @@ module Metrika
       def counter_stat_sources_tags_path
         "/stat/sources/tags"
       end
+
+      # Content
+      def get_counter_stat_content_popular(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_content_popular_path, params.merge(:id => id))
+      end
+
+      def counter_stat_content_popular_path
+        "/stat/content/popular"
+      end
+
+      def get_counter_stat_content_entrance(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_content_entrance_path, params.merge(:id => id))
+      end
+
+      def counter_stat_content_entrance_path
+        "/stat/content/entrance"
+      end
+
+      def get_counter_stat_content_exit(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_content_exit_path, params.merge(:id => id))
+      end
+
+      def counter_stat_content_exit_path
+        "/stat/content/exit"
+      end
+
+      def get_counter_stat_content_titles(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_content_titles_path, params.merge(:id => id))
+      end
+
+      def counter_stat_content_titles_path
+        "/stat/content/titles"
+      end
+
+      def get_counter_stat_content_url_param(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_content_url_param_path, params.merge(:id => id))
+      end
+
+      def counter_stat_content_url_param_path
+        "/stat/content/url_param"
+      end
+
+      def get_counter_stat_content_user_vars(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_content_user_vars_path, params.merge(:id => id))
+      end
+
+      def counter_stat_content_user_vars_path
+        "/stat/content/user_vars"
+      end
+
+      def get_counter_stat_content_ecommerce(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_content_ecommerce_path, params.merge(:id => id))
+      end
+
+      def counter_stat_content_ecommerce_path
+        "/stat/content/ecommerce"
+      end
+
+      # Geo
+      def get_counter_stat_geo(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_geo_path, params.merge(:id => id))
+      end
+
+      def counter_stat_geo_path
+        "/stat/geo"
+      end
+
+      # Demography
+      def get_counter_stat_demography_age_gender(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_demography_age_gender_path, params.merge(:id => id))
+      end
+
+      def counter_stat_demography_age_gender_path
+        "/stat/demography/age_gender"
+      end
+
+      def get_counter_stat_demography_structure(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_demography_structure_path, params.merge(:id => id))
+      end
+
+      def counter_stat_demography_structure_path
+        "/stat/demography/structure"
+      end
+
+      # Tech
+      def get_counter_stat_tech_browsers(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_tech_browsers_path, params.merge(:id => id))
+      end
+
+      def counter_stat_tech_browsers_path
+        "/stat/tech/browsers"
+      end
+
+      def get_counter_stat_tech_os(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_tech_os_path, params.merge(:id => id))
+      end
+
+      def counter_stat_tech_os_path
+        "/stat/tech/os"
+      end
+
+      def get_counter_stat_tech_display(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_tech_display_path, params.merge(:id => id))
+      end
+
+      def counter_stat_tech_display_path
+        "/stat/tech/display"
+      end
+
+      def get_counter_stat_tech_mobile(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_tech_mobile_path, params.merge(:id => id))
+      end
+
+      def counter_stat_tech_mobile_path
+        "/stat/tech/mobile"
+      end
+
+      def get_counter_stat_tech_flash(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_tech_flash_path, params.merge(:id => id))
+      end
+
+      def counter_stat_tech_flash_path
+        "/stat/tech/flash"
+      end
+
+      def get_counter_stat_tech_silverlight(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_tech_silverlight_path, params.merge(:id => id))
+      end
+
+      def counter_stat_tech_silverlight_path
+        "/stat/tech/silverlight"
+      end
+
+      def get_counter_stat_tech_dotnet(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_tech_dotnet_path, params.merge(:id => id))
+      end
+
+      def counter_stat_tech_dotnet_path
+        "/stat/tech/dotnet"
+      end
+
+      def get_counter_stat_tech_java(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_tech_java_path, params.merge(:id => id))
+      end
+
+      def counter_stat_tech_java_path
+        "/stat/tech/java"
+      end
+
+      def get_counter_stat_tech_cookies(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_tech_cookies_path, params.merge(:id => id))
+      end
+
+      def counter_stat_tech_cookies_path
+        "/stat/tech/cookies"
+      end
+
+      def get_counter_stat_tech_javascript(id, params = {})
+        params = self.format_params(params)
+
+        self.get(self.counter_stat_tech_javascript_path, params.merge(:id => id))
+      end
+
+      def counter_stat_tech_javascript_path
+        "/stat/tech/javascript"
+      end
     end
   end
 end
